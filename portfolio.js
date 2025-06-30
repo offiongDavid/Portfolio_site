2a5170630a8f617c3c4f78de505ea7779630a597
Offiong David <offiongd400@gmail.com>
2025-06-30 22:57:30 +0100

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+   const menuIcon = document.getElementById('menuBar');
+    const navLinks = document.getElementById('navBar');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+
+
+
+
+
